6cf7c5a60001e8f55457f7dc96aa12787ce55843
raph_b <bmiggi@gmail.com>
2021-02-08 21:35:20 +1100

    init React config + database connection

---
+++ Talent-Onboarding/Talent-Onboarding/Models/Sales.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Talent_Onboarding.Models
+{
+    public partial class Sales
+    {
+        public int Id { get; set; }
+        public int? Productid { get; set; }
+        public int? Customerid { get; set; }
+        public int? Storeid { get; set; }
+        public DateTime? Datesold { get; set; }
+
+        public virtual Customer Customer { get; set; }
+        public virtual Product Product { get; set; }
+        public virtual Store Store { get; set; }
+    }
+}
---
+++ Talent-Onboarding/Talent-Onboarding/Models/mvptalentContext.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Talent_Onboarding.Models
+{
+    public partial class mvptalentContext : DbContext
+    {
+        public mvptalentContext()
+        {
+        }
+
+        public mvptalentContext(DbContextOptions<mvptalentContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Customer> Customer { get; set; }
+        public virtual DbSet<Product> Product { get; set; }
+        public virtual DbSet<Sales> Sales { get; set; }
+        public virtual DbSet<Store> Store { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=DESKTOP-CEINO38;Database=mvptalent;Integrated Security=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Address)
+                    .HasColumnName("address")
+                    .HasMaxLength(300)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Name)
+                    .HasColumnName("name")
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Name)
+                    .HasColumnName("name")
+                    .HasMaxLength(200)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Price)
+                    .HasColumnName("price")
+                    .HasColumnType("decimal(6, 2)");
+            });
+
+            modelBuilder.Entity<Sales>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Customerid).HasColumnName("customerid");
+
+                entity.Property(e => e.Datesold)
+                    .HasColumnName("datesold")
+                    .HasColumnType("date");
+
+                entity.Property(e => e.Productid).HasColumnName("productid");
+
+                entity.Property(e => e.Storeid).HasColumnName("storeid");
+
+                entity.HasOne(d => d.Customer)
+                    .WithMany(p => p.Sales)
+                    .HasForeignKey(d => d.Customerid)
+                    .HasConstraintName("fk_customerid");
+
+                entity.HasOne(d => d.Product)
+                    .WithMany(p => p.Sales)
+                    .HasForeignKey(d => d.Productid)
+                    .HasConstraintName("fk_productid");
+
+                entity.HasOne(d => d.Store)
+                    .WithMany(p => p.Sales)
+                    .HasForeignKey(d => d.Storeid)
+                    .HasConstraintName("fk_storeid");
+            });
+
+            modelBuilder.Entity<Store>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Address)
+                    .HasColumnName("address")
+                    .HasMaxLength(300)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Name)
+                    .HasColumnName("name")
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
